d7da00e807e9f76204e78222d913d9c74431d208
opadalka <alexandra.padalka@gmail.com>
2025-03-13 11:58:31 +0100

    added homework15

--- lesson_15/hw_15.py
+++ lesson_15/hw_15.py
@@ -13,3 +13,23 @@
     кут_б обчислюється автоматично.
     Для встановлення значень атрибутів використовуйте метод __setattr__.
 """
+
+class Romb():
+    def __setattr__(self, name, value): 
+        if name == 'side_a':
+           if value > 0:
+                super().__setattr__(name, value)
+           else:
+                super().__setattr__(name, value)
+                print("Side A cannot be 0")
+        if name == 'angle_a':
+            if value == 90 and self.side_a > 0:
+                super().__setattr__(name, value)
+                print(f"This is romb, because angle a is {value} and angle b is {180-value} and side a is {self.side_a}")
+            else:
+                super().__setattr__(name, value)
+                print(f"This is NOT romb, because angle a is {value} and/or side a is {self.side_a}")
+
+romb_instance = Romb()
+setattr(romb_instance, 'side_a', 10)
+setattr(romb_instance, 'angle_a', 90)
